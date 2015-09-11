8c134af0e6e40f60453ad112882e9a419966ab20
kpagcha <4d.garcia6@gmail.com>
2015-09-11 16:16:48 +0200

    Edit mp3 upload to episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,6 +1,10 @@
 class Episode < ActiveRecord::Base
-  belongs_to :podcast
+	belongs_to :podcast
 
-  has_attached_file :thumbnail, styles: { large: "1000x1000#", medium: "500x500#" }
+  	has_attached_file :thumbnail, styles: { large: "1000x1000#", medium: "500x500#" }
   	validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
+
+  	has_attached_file :mp3
+  	validates_attachment_content_type :mp3, content_type: [ "audio/mpeg", "audio/mp3" ]
+  	validates_attachment_file_name :mp3, matches: [ /mp3\Z/ ]
 end
