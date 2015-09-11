db1f66b342714a31fe096a10adc272d208beb430
kpagcha <4d.garcia6@gmail.com>
2015-09-11 10:49:30 +0200

    Added dashboard for user

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,10 +1,29 @@
 class PodcastsController < ApplicationController
+	before_action :find_podcast, only: [ :show, :dashboard ]
+	before_action :find_episodes, only: [ :show, :dashboard ]
+
 	def index
 		@podcasts = Podcast.all.order("created_at desc")
 	end
 
 	def show
-		@podcast = Podcast.find(params[:id])
+	end
+
+	def dashboard
+	end
+
+	private
+
+	def find_podcast
+		if params[:id].nil?
+			@podcast = current_podcast
+			100.times{puts @podcast.id}
+		else
+			@podcast = Podcast.find(params[:id])
+		end
+	end
+
+	def find_episodes
 		@episodes = @podcast.episodes.order("created_at desc")
 	end
 end
